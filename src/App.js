d46730d3fe613e4a8d3ae469df907abfe1145942
shivamPUNDIR <47301925+shivamPUNDIR@users.noreply.github.com>
2019-10-28 23:45:18 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+// import React from 'react';
+import React,{Component} from 'react'
+import Navbar from './components/layout/Navbar'
 import './App.css';
 //default app
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from react.</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello from react.</h1>
+//     </div>
+//   );
+// }
+//React.Fragment===Ghost elementor===<>  <>
+class App extends Component {
+    
+     render(){
+      return(<div className = "App" > 
+      <Navbar></Navbar>
+      </div>)
+     }
+    //   return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hello from React.'),React.createElement('div',{className:'HELLO'},React.createElement('h2',null,'This is nested h2.')))
+  }
 
 export default App;
